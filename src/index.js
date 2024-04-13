9529006ebc87baeded07329f826318d575ebd149
Kenneth <kennethyoung.2960@gmail.com>
2024-04-13 00:08:49 -0500

    2 of 6 tests passed

--- src/index.js
+++ src/index.js
@@ -4,13 +4,27 @@ import "./styles.css";
   Write the addheadings() function here
 */
 
+function addHeadings() {
+  const articles = document.querySelectorAll('.articles article');
+  articles.forEach(article => {
+    const heading = document.createElement('h2');
+    heading.textContent = article.textContent;
+    article.innerHTML = '';
+    article.appendChild(heading);
+  });
+}
+
 /**
   Write the styleTutorialsAndArticles() function here
 */
 
+function styleTutorialsAndArticles() {}
+
 /**
   Write the separateAllTutorials() function here
 */
+
+function separateAllTutorials () {}
 
 /**
   No need to edit the following
